12970d5049548e4ebf463c7aacde1148c5b847a0
MaxCan-Code <35848843+MaxCan-Code@users.noreply.github.com>
2021-04-02 06:06:15 -0400

    functionalize

--- program20.py
+++ program20.py
@@ -12,33 +12,38 @@ from scipy.interpolate import interp2d
 from numpy.linalg import matrix_power
 
 def solve_and_plot(sp_diff, N):
-# Grid and initital data
-N = 24
-x = cos(pi*arange(0,N+1)/N)
-y = x.T
-dt = 6.0/N**2
-[xx,yy] = meshgrid(x,y)
-plotgap = int(round((1./3)/dt))
-dt = (1./3)/plotgap
-vv = exp(-40*((xx-0.4)**2+yy**2))
-vvold = vv
+    # Grid and initital data
+    # N = 24
+    x = cos(pi*arange(0,N+1)/N)
+    y = x.T
+    dt = 6.0/N**2
+    [xx,yy] = meshgrid(x,y)
+    plotgap = int(round((1./3)/dt))
+    dt = (1./3)/plotgap
+    vv = exp(-40*((xx-0.4)**2+yy**2))
+    vvold = vv
 
-# Time-stepping by leap frog formula:
-fig = plt.figure()
-for n in range(0,3*plotgap+1):
-    t = n*dt
-    if (((n+0.5)%plotgap) < 1):        #plots at multiplies of t = 1/3
-        i = int(float(n)/plotgap) + 1
-        ar = arange(-1,1+1./16,1./16)
-        [xxx,yyy] = meshgrid(ar,ar)
-        vvv = interp2d(x, y, vv, kind='cubic')
-        ax = fig.add_subplot(2,2,i, projection='3d')
-        ax.plot_surface(xxx, yyy, vvv(ar,ar), rstride=1, cstride=1, cmap="coolwarm", alpha=0.3)
-        ax.set_xlim(-1, 1)
-        ax.set_ylim(-1, 1)
-        ax.set_zlim(-0.15, 1)
-        ax.text2D(0.05, 0.95, 't = '+str(round(t, 2)), transform=ax.transAxes)
+    # Time-stepping by leap frog formula:
+    fig = plt.figure()
+    for n in range(0,3*plotgap+1):
+        t = n*dt
+        if (((n+0.5)%plotgap) < 1):        #plots at multiplies of t = 1/3
+            i = int(float(n)/plotgap) + 1
+            ar = arange(-1,1+1./16,1./16)
+            [xxx,yyy] = meshgrid(ar,ar)
+            vvv = interp2d(x, y, vv, kind='cubic')
+            ax = fig.add_subplot(2,2,i, projection='3d')
+            ax.plot_surface(xxx, yyy, vvv(ar,ar), rstride=1, cstride=1, cmap="coolwarm", alpha=0.3)
+            ax.set_xlim(-1, 1)
+            ax.set_ylim(-1, 1)
+            ax.set_zlim(-0.15, 1)
+            ax.text2D(0.05, 0.95, 't = '+str(round(t, 2)), transform=ax.transAxes)
 
+
+        Δvv= sp_diff(N, vv, x, y)
+        # Δvv = fft2D(N, vv, x, y)
+        vvold, vv = time_step(vvold, vv, Δvv, dt)
+    plt.show()
 def fft2D(N, vv, x, y):
     uxx = zeros((N+1,N+1))
     uyy = zeros((N+1,N+1))
@@ -71,10 +76,6 @@ def time_step(vvold, vv, Δvv, dt):
 
     return vvold,vv
 
-    # Δvv= sp_diff(N, vv, x, y)
-    Δvv = fft2D(N, vv, x, y)
-    vvold, vv = time_step(vvold, vv, Δvv, dt)
-plt.show()
 
 def main():
     N = 24
