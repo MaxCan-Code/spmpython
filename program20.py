8951fc34492e7e1e91bc521e6519d8673e10409c
MaxCan-Code <35848843+MaxCan-Code@users.noreply.github.com>
2021-04-12 11:55:58 -0400

    converted to heat eqn on annulus

--- program20.py
+++ program20.py
@@ -11,7 +11,7 @@ from mpl_toolkits.mplot3d import axes3d
 from scipy.interpolate import interp2d
 from numpy.linalg import matrix_power
 
-def solve_and_plot(sp_diff, N):
+def solve_and_plot(sp_diff, N, partial_t):
     # Grid and initital data
     # N = 24
     x = cos(pi*arange(0,N+1)/N)
@@ -19,34 +19,43 @@ def solve_and_plot(sp_diff, N):
     dt = 6.0/N**2
     [xx,yy] = meshgrid(x,y)
     plotgap = int(round((1./3)/dt))
-    dt = (1./3)/plotgap
-    vv = exp(-40*((xx-0.4)**2+yy**2))
+    dt = (1/3)/plotgap * 1e-15
+    dt *= .6
+    vv = exp(-xx ** 2)
     vvold = vv
 
     # Time-stepping by leap frog formula:
     fig = plt.figure()
+    plt.title(partial_t+f" dt_ex = {dt:.2e}")
     for n in range(0,3*plotgap+1):
         t = n*dt
         if (((n+0.5)%plotgap) < 1):        #plots at multiplies of t = 1/3
             i = int(float(n)/plotgap) + 1
             ar = arange(-1,1+1./16,1./16)
-            [xxx,yyy] = meshgrid(ar,ar)
+            [rr, θθ] = meshgrid(ar,ar)
+            xxx = rr * cos(θθ)
+            yyy = rr * sin(θθ)
             vvv = interp2d(x, y, vv, kind='cubic')
             ax = fig.add_subplot(2,2,i, projection='3d')
             ax.plot_surface(xxx, yyy, vvv(ar,ar), rstride=1, cstride=1, cmap="coolwarm", alpha=0.3)
-            ax.set_xlim(-1, 1)
-            ax.set_ylim(-1, 1)
-            ax.set_zlim(-0.15, 1)
-            ax.text2D(0.05, 0.95, 't = '+str(round(t, 2)), transform=ax.transAxes)
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('u')
+            ax.set_zlim(-.7, .7)
+            ax.text2D(0.05, 0.95, f'{t = :.2e}', transform=ax.transAxes)
 
 
-        Δvv= sp_diff(N, vv, x, y)
-        # Δvv = fft2D(N, vv, x, y)
-        vvold, vv = time_step(vvold, vv, Δvv, dt)
+
+        Δvv = sp_diff(N, vv, x, y)
+        vvold, vv = time_step(vvold, vv, Δvv, dt, partial_t)
+    plt.savefig("figs-fft/"+partial_t+".jpg")
     plt.show()
+
+
 def fft2D(N, vv, x, y):
-    uxx = zeros((N+1,N+1))
-    uyy = zeros((N+1,N+1))
+    urr = zeros((N+1,N+1))
+    r_1ur = zeros((N+1,N+1))
+    r_2uθθ = zeros((N+1,N+1))
     ii = arange(1,N)
     for i in range(1,N+1):          # 2nd derivs wrt x in each row
         v = vv[i,:]
@@ -54,33 +63,39 @@ def fft2D(N, vv, x, y):
         U = real(fft.fft(V))
         W1 = real(fft.ifft(1j*hstack([arange(0,N), 0, arange(1-N,0)]).T*U))     # diff wrt theta
         W2 = real(fft.ifft(-hstack([arange(0,N+1), arange(1-N,0)]).T**2*U))       # diff**2 wrt theta
-        uxx[i,ii] = W2[ii]/(1-x[ii]**2) - x[ii]*W1[ii]/(1-x[ii]**2)**(3./2)
+        urr[i,ii] = W2[ii]/(1-x[ii]**2) - x[ii]*W1[ii]/(1-x[ii]**2)**(3./2)
+        r_1ur[i,ii] = (1/x[ii])*-W1[ii]/(1-x[ii]**2)**(1/2)
     for k in range(1,N+1):
         v = vv[:,k]
         V = hstack([v,flipud(v[ii])])
         U = real(fft.fft(V))
         W1 = real(fft.ifft(1j*hstack([arange(0,N), 0, arange(1-N,0)]).T*U))     # diff wrt theta
         W2 = real(fft.ifft(-hstack([arange(0,N+1), arange(1-N,0)]).T**2*U))       # diff**2 wrt theta
-        uyy[ii,k] = W2[ii]/(1-y[ii]**2) - y[ii]*W1[ii]/(1-y[ii]**2)**(3./2)
-    return uxx+uyy
+        r_2uθθ[ii,k] = (1/y[ii]**2)*W2[ii]
+    return urr + r_1ur + r_2uθθ
 
-def time_step(vvold, vv, Δvv, dt):
-    # leap frog
-    vvnew = 2*vv - vvold + Δvv*dt**2
-    vvold = vv
-    vv = vvnew
+def time_step(vvold, vv, Δvv, dt, method):
+    # dt_im = dt * 60
+    dt_ex = dt
+    # dt_ex = dt * 1e-15
+    # dt_ex *= .6
 
-    # Euler (fwd)
-    # dt *= 8.5e-3
-    # vv += dt * Δvv
+    if method == "leap frog":
+        vvnew = vvold + Δvv * dt_ex * 2
+        vvold = vv
+        vv = vvnew
+
+    if method == "forward Euler":
+        vv += dt_ex * Δvv
 
     return vvold,vv
 
 
 def main():
     N = 24
-    solve_and_plot(fft2D, N)
+    %time solve_and_plot(fft2D, N, "forward Euler")
+    %time solve_and_plot(fft2D, N, "leap frog")
 
     
-main()
+%time main()
 
--- program29.py
+++ program29.py
@@ -17,7 +17,16 @@ def matrix2D(N, M):
 
     # N = 31
     N2 = (N - 1) // 2
-    [D, r] = cheb(N)
+    [D,x] = cheb(N)
+
+    # annulus
+    r_1 = 1 /4
+    r_2 = 1
+
+    a = 2 * r_1 / (r_2 - r_1) + 1
+    b = 2 / (r_2 - r_1)
+    r = (x + a) / b
+
     D2 = D @ D
     D1 = D2[1:N2 + 1, 1:N2 + 1]
     D2 = D2[1:N2 + 1, N - 1:N2:-1]
@@ -45,8 +54,30 @@ def matrix2D(N, M):
     return r, θ, L
 
 
+def time_step(vvold, L, vv, dt, method):
+    # Δvv = L @ vv
+    dt_im = dt * 60
+    dt_ex = dt * 2e-4
 
-def solve_and_plot(sp_diff, N, M):
+    if method == "leap frog":
+        vvnew = vvold + (L @ vv) * dt_ex * 2
+        vvold = vv
+        vv = vvnew
+
+    if method == "forward Euler":
+        vv += dt_ex * (L @ vv)
+
+    if method == "backward Euler":
+        vv = linalg.solve(1 - dt_im * L, vv)
+
+    if method == "Crank-Nicolson":
+        _ = (dt_im / 2) * L  # not sure what to name it
+        vv = linalg.solve(1 - _, (1 + _) @ vv)
+
+    return vvold, vv
+
+
+def solve_and_plot(sp_diff, N, M, partial_t):
     N2 = (N - 1) // 2
     r, θ, L = sp_diff(N, M)
 
@@ -54,35 +85,53 @@ def solve_and_plot(sp_diff, N, M):
     [rr, θθ] = meshgrid(r[1:N2 + 1], θ)
     rr = hstack(stack(rr[:], axis=-1))
     θθ = hstack(stack(θθ[:], axis=-1))
-    f = -rr**2 * sin(θθ / 2)**4 + sin(6 * θθ) * cos(θθ / 2)**2
-    u = linalg.solve(L, f)
+    f = exp(-rr ** 2)
+    u = f
+    uold = u
 
+    dt = 6.0/N**2
+    plotgap = int(round((1./3)/dt))
+    dt = (1./3)/plotgap
+
+    # Time-stepping by leap frog formula:
+    fig = plt.figure()
+    plt.title(partial_t+f" dt_im = {dt * 60:.2e}, dt_ex = {dt * 2e-4:.2e}")
+    
+    for n in range(0,3*plotgap+1):
+        t = n*dt
+        if (((n+0.5)%plotgap) < 1):        #plots at multiplies of t = 1/3
+            i = int(float(n)/plotgap) + 1
     # Reshape results onto 2D grid and plot them:
-    u = reshape(u, (N2, M)).T
-    u2 = vstack((u[M - 1, :], u[0:M - 1, :]))
-    u = hstack([zeros((M, 1)), u2])
+    uu = reshape(u, (N2, M)).T
+    u2 = vstack((uu[M - 1, :], uu[0:M - 1, :]))
+    uu = hstack([zeros((M, 1)), u2])
     [rr, θθ] = meshgrid(r[0:N2 + 1], hstack([θ[M - 1], θ[0:M - 1]]))
     xx = rr * cos(θθ)
     yy = rr * sin(θθ)
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(xx, yy, u, rstride=1, cstride=1, cmap='coolwarm', alpha=.3)
+    ax = fig.add_subplot(2,2,i, projection='3d')
+    ax.text2D(0.05, 0.95, f'{t = :.2e}', transform=ax.transAxes)
+    ax.plot_surface(xx, yy, uu, rstride=1, cstride=1, cmap='coolwarm', alpha=.3)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('u')
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
-    ax.set_zlim(-.01, .05)
-    ax.set_zticks([-.01, .05])
-    plt.show()
+    ax.set_zlim(-.7, .7)
 
+        # Δvv = sp_diff(N, vv, x, y)
+        uold, u = time_step(uold, L, u, dt, partial_t)
+    plt.savefig("figs-matrix/"+partial_t+".jpg")
+    plt.show()
     
 
 def main():
     N = 31
     M = 40
-    solve_and_plot(matrix2D, *(N, M))
+    %time solve_and_plot(matrix2D, *(N, M), "forward Euler")
+    %time solve_and_plot(matrix2D, *(N, M), "leap frog")
+    %time solve_and_plot(matrix2D, *(N, M), "backward Euler")
+    %time solve_and_plot(matrix2D, *(N, M), "Crank-Nicolson")
+    # %time solve_and_plot(fft2D, *(N, M), "forward Euler")
+    # %time solve_and_plot(fft2D, *(N, M), "leap frog")
 
     
-main()
+%time main()
 
